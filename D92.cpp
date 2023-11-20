8b4cc7bf3d4b206d131dc5160966ab61b735e369
Vishwas kumar <vishwas101kumar@gmail.com>
2023-11-21 00:31:47 +0530

    D92

---
+++ D92.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int left, int right)
+    {
+        int ans = left;
+        int count = 0;
+        while (left != right)
+        {
+            left = left >> 1;
+            right = right >> 1;
+            count++;
+        }
+
+        cout << "count : " << count << endl;
+        cout << "left : " << left << endl;
+
+        for (int i = 1; i <= count; i++)
+        {
+            left = (left << 1);
+        }
+
+        return left;
+    }
+};
+
+int main()
+{
+    system("cls");
+    Solution s;
+    int left = 9;
+    int right = 15;
+    cout << "ans: " << s.rangeBitwiseAnd(left, right);
+}
